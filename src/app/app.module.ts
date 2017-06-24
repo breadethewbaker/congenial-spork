8ed44df4e12c88122932a1c9f424cf3a5c303ecc
breadethewbaker <bmbaker@buffalo.edu>
2017-06-23 22:44:17 -0400

    added express backend in server folder and server.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ngx-bootstrap/ng2-bootstrap';
+
+import { AppRoutingModule }     from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { EventDisplayComponent } from './event-display/event-display.component';
@@ -10,8 +13,6 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
 import { EventService } from './event.service';
 import { HomeViewComponent } from './home-view/home-view.component';
 import { AddEventComponent } from './add-event/add-event.component';
-
-import { AppRoutingModule }     from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { AppRoutingModule }     from './app-routing/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule,
     AlertModule.forRoot(),
   ],
